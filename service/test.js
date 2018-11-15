f2ddabc00e53f173e32da12fd56ac905c2a9f26c
jaypinedabengco <jaybengcodeveloper@gmail.com>
2018-11-15 17:56:35 +0800

    ADD es6 & twick mocha config to allow es6 on testing

--- service/test.js
+++ service/test.js
@@ -1,5 +1,22 @@
-const testServiceFunctionThatDoSomething = async (number1, number2) => {
+/**
+ *
+ * @param {*} number1
+ * @param {*} number2
+ */
+async function testServiceFunctionThatDoSomething(number1, number2) {
   return number1 + number2
 }
 
-exports.testServiceFunctionThatDoSomething = testServiceFunctionThatDoSomething
+/**
+ *
+ * @param {*} number1
+ * @param {*} number2
+ */
+async function testServiceFunctionThatDoSomething2(number1, number2) {
+  return number1 + number2
+}
+
+export default {
+  testServiceFunctionThatDoSomething,
+  testServiceFunctionThatDoSomething2
+}
--- test/unit/service/test.js
+++ test/unit/service/test.js
@@ -1,12 +1,8 @@
-var expect = require('chai').expect
-var test = require('./../../../service/test.js')
+import { expect } from 'chai'
+import test from './../../../service/test.js'
 
-describe(`Test Service`, () => {
-  describe(`Test 'testServiceFunctionThatDoSomething' function`, () => {
-    it(`should exist`, () => {
-      expect(!test.testServiceFunctionThatDoSomething).to.not.equal(true)
-    })
-
+describe.only(`Test Service`, () => {
+  describe(`testServiceFunctionThatDoSomething function`, () => {
     it(`should return a promise`, () => {
       const promise = test.testServiceFunctionThatDoSomething()
       expect(promise).to.be.an.instanceof(Promise)
@@ -21,5 +17,32 @@ describe(`Test Service`, () => {
       const result = await test.testServiceFunctionThatDoSomething(3, 2)
       expect(result).to.equal(5)
     })
+  })
+
+  describe(`testServiceFunctionThatDoSomething2 function`, () => {
+    it(`should exist`, () => {
+      expect(!test.testServiceFunctionThatDoSomething2).to.not.equal(true)
+    })
+
+    it(`should return a promise`, () => {
+      const promise = test.testServiceFunctionThatDoSomething2()
+      expect(promise).to.be.an.instanceof(Promise)
+    })
+
+    it(`should return 2 when passed with 1,1 on param`, async () => {
+      const result = await test.testServiceFunctionThatDoSomething2(1, 1)
+      expect(result).to.equal(2)
+    })
+
+    it(`should return 5 when passed with 3,2 on param`, async () => {
+      const result = await test.testServiceFunctionThatDoSomething2(3, 2)
+      expect(result).to.equal(5)
+    })
+  })
+
+  describe(`addUserFromTestDB function`, () => {
+    // get user dao
+    // validate passed value if valid
+    it(`should validate passed value as valid`)
   })
 })
